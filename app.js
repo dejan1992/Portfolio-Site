d473928c6c5f3a85bb896943ce7e104dfc7881cb
Dejan Cetojevic <dejan92ceta@live.com>
2021-08-16 16:29:23 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector("nav");
+let scrolled = false;
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    nav.classList.remove("top");
+    if (!scrolled) {
+      nav.style.transform = "translateY(-140px)"
+    }
+    setTimeout(function () {
+      nav.style.transform = "translateY(0)";
+      scrolled = true;
+    }, 200)
+  } else {
+    nav.classList.add("top");
+    scrolled = false;
+  }
+}
+
+
